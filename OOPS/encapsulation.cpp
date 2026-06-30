12c950948af832ad21dcd0996413b7fac4658718
avserver-16 <avish.vijay2021@gmail.com>
2026-07-01 00:57:37 +0530

    feat: OOPS: added encapsulation

---
+++ OOPS/encapsulation.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+class Car
+{
+
+public:
+    virtual void startEngine() = 0;
+    virtual void shiftGear() = 0;
+    virtual void accelerate() = 0;
+    virtual void brake() = 0;
+    virtual void stopEngine() = 0;
+
+    virtual ~Car() {}
+};
+
+class SportsCar : public Car
+{
+private:
+    string brand, model;
+    bool isEnginestart;
+    int currentSpeed, currentGear;
+
+public:
+    SportsCar(string brand, string model)
+    {
+        this->brand = brand;
+        this->model = model;
+        this->isEnginestart = false;
+        this->currentSpeed = 0;
+        this->currentGear = 0;
+    }
+    void startEngine()
+    {
+        isEnginestart = true;
+        cout << "SportsCar started" << endl;
+    }
+    void shiftGear()
+    {
+        if (!isEnginestart)
+        {
+            cout << "Start the engine to shift gears" << endl;
+            return;
+        }
+        currentGear++;
+        cout << "Gear shifted from" << currentGear - 1 << "to" << currentGear << endl;
+    }
+    void accelerate()
+    {
+        if (!isEnginestart)
+        {
+            cout << "Start the engine to accelerate" << endl;
+            return;
+        }
+        currentSpeed++;
+        cout << "Speed went from " << currentSpeed - 1 << " to " << currentSpeed + 1 << endl;
+    }
+    void brake()
+    {
+        if (currentSpeed == 0)
+        {
+            cout << "Car is stopped " << endl;
+            return;
+        }
+        if (!isEnginestart)
+        {
+            cout << "Start the engine to accelerate and then brake" << endl;
+            return;
+        }
+        currentSpeed--;
+        cout << "Speed went from " << currentSpeed + 1 << " to " << currentSpeed + 1 << endl;
+    }
+    void stopEngine()
+    {
+        if (!isEnginestart)
+        {
+            cout << "Engine stopped" << endl;
+            return;
+        }
+        currentGear = 0;
+        currentSpeed = 0;
+        isEnginestart = false;
+        cout << "Engine stopped" << endl;
+    }
+    int getSpeed()
+    {
+        cout << "Current speed " << currentSpeed << endl;
+    }
+};
+
+int main()
+{
+    Car *obj = new SportsCar("Toyota", "Landcruiser");
+    obj->startEngine();
+    obj->shiftGear();
+    obj->accelerate();
+    obj->accelerate();
+    obj->accelerate();
+    obj->accelerate();
+    obj->accelerate();
+    obj->accelerate();
+    obj->accelerate();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    obj->brake();
+    // obj->currentSpeed cannot accesss
+    obj->stopEngine();
+    delete obj;
+    return 0;
+}
---
+++ OOPS/redme.abs.md
@@ -0,0 +1,22 @@
+SportsCar started
+Gear shifted from0to1
+Speed went from 0 to 2
+Speed went from 1 to 3
+Speed went from 2 to 4
+Speed went from 3 to 5
+Speed went from 4 to 6
+Speed went from 5 to 7
+Speed went from 6 to 8
+Speed went from 7 to 7
+Speed went from 6 to 6
+Speed went from 5 to 5
+Speed went from 4 to 4
+Speed went from 3 to 3
+Speed went from 2 to 2
+Speed went from 1 to 1
+Car is stopped 
+Car is stopped 
+Car is stopped 
+Car is stopped 
+Car is stopped 
+Engine stopped
